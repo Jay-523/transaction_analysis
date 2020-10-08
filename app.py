4db3a3ab9a3169d03c605e2a8ccc2e740cb9de25
Jay-523 <60832779+Jay-523@users.noreply.github.com>
2020-10-08 23:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def get_kpi():
     content = request.get_json(silent= True)
     content = json.loads(content)
     data = content[1]
+    key = content[3]
+    
+    if(key != 'iorusjkldfh#7lkhfa#adf88ayhwgfhhdfhthweasdfasjytk'):
+        return "<h1> Could not verify</h1>"
     d = pd.DataFrame(data)
     savings,inc_grad, s_grad, rs_grad,inc_median, s_median, rs_median, p, df = get_basic_kpis(d)
     return {"income_gradient": inc_grad, "savings_gradient": s_grad, "cumulative_savings_gradient": s_grad, "median_income": inc_median, "median_savings": s_median, "cumulative_savings_median": rs_median, "peristance": p}
