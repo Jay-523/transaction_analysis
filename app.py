08dae513f6cdcb6496246a5c0ce440bde14a0c94
Jay-523 <60832779+Jay-523@users.noreply.github.com>
2020-10-09 22:02:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>hey there</h1>"
+    return "<h1>FlatFolder KPI</h1>"
 
 
 @app.route('/api/getkpi', methods = ['GET', 'POST'])
 
 def get_kpi():
+    
     content = request.get_json(silent= True)
+    
     content = json.loads(content)
-    data = content[1]
-    key = content[3]
+    data = content['data']
+    
+    print(type(content))
+    key = content['api_key']
     
     if(key != 'iorusjkldfh#7lkhfa#adf88ayhwgfhhdfhthweasdfasjytk'):
-        return "<h1> Could not verify</h1>"
-    d = pd.DataFrame(data)
+        return {'login': 'could not verify'}
+    d = pd.DataFrame(data[1])
     savings,inc_grad, s_grad, rs_grad,inc_median, s_median, rs_median, p, df = get_basic_kpis(d)
     return {"income_gradient": inc_grad, "savings_gradient": s_grad, "cumulative_savings_gradient": s_grad, "median_income": inc_median, "median_savings": s_median, "cumulative_savings_median": rs_median, "peristance": p}
     
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
 
