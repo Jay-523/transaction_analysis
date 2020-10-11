b343e3d6b839d679abb19c6322b8edfa1a5d116f
Jay-523 <60832779+Jay-523@users.noreply.github.com>
2020-10-11 22:02:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_kpi():
     
     content = request.get_json(silent= True)
     
-    content = json.loads(content)
+    #content = json.loads(content)
     data = content['data']
     
     
@@ -29,7 +29,7 @@ def get_kpi():
     #savings,inc_grad, s_grad, rs_grad,inc_median, s_median, rs_median, p, df = get_basic_kpis(d)
     #return {"income_gradient": inc_grad, "savings_gradient": s_grad, "cumulative_savings_gradient": s_grad, "median_income": inc_median, "median_savings": s_median, "cumulative_savings_median": rs_median, "peristance": p}
     
-    return content
+    return str(type(content))
 
 if __name__ == "__main__":
     app.run(debug = True)
