419075cf0caf3146feae9a36da0f5342b54ddaf6
Jay-523 <60832779+Jay-523@users.noreply.github.com>
2020-10-11 18:22:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,13 @@ def get_kpi():
     
     key = content['api_key']
     
-    if(key != 'iorusjkldfh#7lkhfa#adf88ayhwgfhhdfhthweasdfasjytk'):
-        return {'login': 'could not verify'}
-    d = pd.DataFrame(data[1])
-    savings,inc_grad, s_grad, rs_grad,inc_median, s_median, rs_median, p, df = get_basic_kpis(d)
-    return {"income_gradient": inc_grad, "savings_gradient": s_grad, "cumulative_savings_gradient": s_grad, "median_income": inc_median, "median_savings": s_median, "cumulative_savings_median": rs_median, "peristance": p}
+    #if(key != 'iorusjkldfh#7lkhfa#adf88ayhwgfhhdfhthweasdfasjytk'):
+    #    return {'login': 'could not verify'}
+    #d = pd.DataFrame(data[1])
+    #savings,inc_grad, s_grad, rs_grad,inc_median, s_median, rs_median, p, df = get_basic_kpis(d)
+    #return {"income_gradient": inc_grad, "savings_gradient": s_grad, "cumulative_savings_gradient": s_grad, "median_income": inc_median, "median_savings": s_median, "cumulative_savings_median": rs_median, "peristance": p}
     
+    return content
 
 if __name__ == "__main__":
     app.run(debug = True)
